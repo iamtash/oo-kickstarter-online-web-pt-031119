f74c5e96687e5963f55dd96c803a92030a0d14e6
Natasha Chernis <natashachernis@gmail.com>
2019-04-12 12:51:12 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project 
+  attr_accessor :name
+  attr_reader :backers 
+
+  def initialize(name)
+    @name = name
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backers << backer 
+  end
+    
+end
